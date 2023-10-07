e846e4998646c7a3f0f968d189918c9854c4bae5
L3Santos <lucasviniciuscruz2020@gmail.com>
2023-10-06 22:52:02 -0300

    atts adds

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,10 +3,16 @@ import Header from '../../components/Header/index';
 import Title from "../../components/Title";
 import { FiSettings, FiUpload } from "react-icons/fi";
 
-import "./profile.css"
+import "./profile.css";
+import { toast } from "react-toastify";
+
 
 import avatar from "../../assets/avatar.png"
-import {AuthContext, storegeUser, setUser, logout } from "../../contexts/auth"
+import {AuthContext} from "../../contexts/auth"
+
+import { db, storage } from "../../services/firebaseConnection"
+import { doc, updateDoc } from "firebase/firestore"
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
 
 export default function Profile() {
 
@@ -36,6 +42,76 @@ export default function Profile() {
         }
     }
 
+
+    async function handlerUpload() {
+        const currentUid = user.uid;
+
+        const uploadRef = ref(storage, `images/${currentUid}/${imgAvatar.naame}`)
+
+        const uploadTask = uploadBytes(uploadRef, imgAvatar)
+        .then((snapshot) => {
+            getDownloadURL(snapshot.ref).then( async (downloadURL) => {
+                let urlFoto = downloadURL;
+
+                const docRef = doc(db, "users", user.uid)
+                await updateDoc(docRef, {
+                    avatarURL: urlFoto,
+                    nome: nome,
+                })
+                .then(() => {
+                    let data = {
+                        ...user,
+                        avatarURL: urlFoto,
+                        nome: nome,
+                    }
+    
+                    setUser(data);
+                    storegeUser(data);
+                    toast.success("Dados atualizados com sucesso")
+                }).catch((error) => {
+                    console.log(error)
+                    toast.error("Erro ao atualizar dados cadastrais! Verifique as informações...")
+                })
+            }).catch((error) => {
+                console.log(error)
+                toast.error("Erro ao processar a atualização dados cadastrais! Verifique as informações...")
+            })
+        })
+        .catch((error) => {
+        toast.error("Erro ao inicializar processo de atualizar dados cadastrais! Verifique as informações...")
+        })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if(imgAvatar === null && nome !== "") {
+            // Atualizar apenas o nome do usuario
+
+            const docRef = doc(db, "users", user.uid)
+            await updateDoc(docRef, {
+                nome: nome, 
+            })
+            .then(() => {
+                let data = {
+                    ...user,
+                    nome: nome,
+                }
+
+                setUser(data);
+                storegeUser(data);
+                toast.success("Dados atualizados com sucesso")
+            })
+            .catch((error) => {
+                console.alert(error)
+                toast.error("Erro ao atualizar dados cadastrais! Verifique as informações...")
+            })
+        } else if (nome !== "" && imgAvatar !==  null) {
+            // Atualizar tanto o nome quanto a foto
+            handlerUpload();
+        }
+    }
+
     return(
         <div>
             <Header/>
@@ -49,7 +125,7 @@ export default function Profile() {
 
             <div className="content">
                 <div className="container">
-                    <form className="form-profile">
+                    <form className="form-profile" onSubmit={handleSubmit}>
                         <label className="label-avatar">
                             <span>
                                 <FiUpload color="#FFFF" size={25}/>
@@ -81,7 +157,7 @@ export default function Profile() {
                             disabled={true}
                         />
 
-                        <button type="submit">Salvar</button>
+                        <button className="submit-add" type="submit">Salvar</button>
                     </form>
                 </div>
 
